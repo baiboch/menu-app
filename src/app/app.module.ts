411fc8bab7af7849fd873a152183ae33fe5c169d
Chingiz Baibogirov <c.baibogirov@platform.live>
2022-05-02 22:11:48 +0200

    add rxjs dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
 import {MainComponent} from './pages/main/main.component';
-import {MainModule} from "./pages/main/main.module";
+import {MainModule} from './pages/main/main.module';
+import {counterReducer} from './pages/main/main.reducer';
 
 @NgModule({
   imports: [
     BrowserModule,
+    StoreModule.forRoot({
+      count: counterReducer,
+    }),
     MainModule,
   ],
   providers: [],
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,13 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Store} from "@ngrx/store";
+import {increment, decrement, reset} from "./main.actions";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'main.component.html',
   styleUrls: ['main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   title = 'test-angular-app';
+  count$: Observable<number>;
 
-  constructor() {
+  constructor(private store: Store<{count: number}>) {
+    this.count$ = store.select('count');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+
+  ngOnInit() {
+
   }
 }

e5d867656edff68fddab5610f33644f1be91298d
Chingiz Baibogirov <c.baibogirov@platform.live>
2022-05-02 22:16:29 +0200

    fix main module state

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {StoreModule} from '@ngrx/store';
 import {MainComponent} from "./main.component";
-import {MainRoutingModule} from "./main-routing.module";
+import {MainRoutingModule} from './main-routing.module';
+import {counterReducer} from './main.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
+    StoreModule.forRoot({
+      count: counterReducer,
+    }),
   ],
   declarations: [
     MainComponent,
